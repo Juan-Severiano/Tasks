acb9a1557082256e337f86164db814d9c2328baf
Francisco Juan <juansoussev@gmail.com>
2025-03-12 17:08:57 -0300

    Creating Icons, changing its names and change colors too

--- Tasks/ContentView.swift
+++ Tasks/ContentView.swift
@@ -10,10 +10,12 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
+            Image(systemName: "microphone.fill")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Hello, world on SwiftUI!")
+            Image(systemName: "microphone.fill")
+                .imageScale(.large)
         }
         .padding()
     }
