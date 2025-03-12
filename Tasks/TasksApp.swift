0ec8b1007e047dec54716713eb4edccde11c0bdc
Francisco Juan <juansoussev@gmail.com>
2025-03-12 16:53:33 -0300

    Initial Commit

---
+++ Tasks/TasksApp.swift
@@ -0,0 +1,17 @@
+//
+//  TasksApp.swift
+//  Tasks
+//
+//  Created by user on 12/03/25.
+//
+
+import SwiftUI
+
+@main
+struct TasksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
